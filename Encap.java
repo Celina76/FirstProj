61018246756bc2647ec79c9034d5b5f84cdbc0e1
Celina76 <celinacelin07@gmail.com>
2016-12-22 12:40:57 +0530

    firstupload

---
+++ Encap.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Appl
+{
+private int sid=10;
+private String sname="hai";
+int getSid()
+{
+return sid;
+}
+String getSname()
+{
+return sname;
+}
+void setSid(int x)
+{
+sid=x;
+}
+void setSname(String n)
+{
+sname=n;
+}
+}
+class Encap
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.next();
+
+Appl a1=new Appl();
+a1.setSid(20);
+System.out.println(a1.getSid());
+a1.setSname(s1);
+System.out.println(a1.getSname());}  }                               
---
+++ Test1.java
@@ -0,0 +1,45 @@
+class Electronics
+{
+void cmpyName()
+{
+System.out.println("Samsung");
+}}
+class Radio extends Electronics
+{
+void speakerFreq()
+{
+System.out.println("Five sound base speakers");
+}
+void cmpyName()
+{
+System.out.println("Panasonic");
+}
+}
+class Tv extends Electronics
+{
+void dispInch()
+{
+System.out.println("48 inch");
+}}
+class Cdplayer extends Electronics
+{
+void mulpDisk()
+{
+System.out.println("more 2 cd can be used");
+}}
+class Test1
+{
+public static void main(String a[])
+{
+Radio r=new Radio();
+Tv t=new Tv();
+Cdplayer c=new Cdplayer();
+r.cmpyName();
+r.speakerFreq();
+t.dispInch();
+t.cmpyName();
+c.mulpDisk();
+c.cmpyName();
+}
+}
+
